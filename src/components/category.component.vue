bb95d993556724fce16fb81e14ea1b388f5b66d1
cuong.hoang <cuong.hoang@devsoft.vn>
2019-09-13 17:42:47 +0700

    add category

---
+++ src/components/category.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="nav justify-content-start">
+    <li class="nav-item" v-for="c in categories">
+      <a class="nav-link" href="#">{{ c.title }}</a>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        { id: 1, title: "Mobile", description: "Mobile category" },
+        { id: 2, title: "Laptop", description: "Laptop category" }
+      ]
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <home-component msg="Welcome to Your Vue.js App" />
+    <div>
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <home-component msg="Welcome to Your Vue.js App" />
+    </div>
+    <div>
+      <category-component></category-component>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Home from "@/components/home.component.vue";
+import Category from "@/components/category.component.vue";
 
 export default {
   name: "Home",
   components: {
-    "home-component": Home
+    "home-component": Home,
+    "category-component": Category
   }
 };
 </script>
